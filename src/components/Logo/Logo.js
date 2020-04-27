0cbbf5f938a0e36b6c088695a860fbdb260c10cb
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-27 15:33:19 +0700

    Create Logo.js

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../../Image/touch logo.png'
+import './Logo.scss'
+
+const logo = (props) => (
+    <div className="Logo" >
+        <img src={Logo} alt="logo" />
+    </div>
+)
+
+export default logo
