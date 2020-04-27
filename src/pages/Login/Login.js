72438f71b7bd625ceef53715936378287e194fd0
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-27 15:33:27 +0700

    Update Login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Logo from '../../components/Logo/Logo'
+import firebase from '../../config/firebaseConfig'
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 class Login extends React.Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
   render() {
-    return <h1>Login Page</h1>
+    return  (
+      <div className="container">
+        <Logo />
+        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+      </div>
+    )
   }
 }
 
