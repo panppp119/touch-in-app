d83de15ff2f4cba1a20383cd122b7440bdab7477
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-28 14:50:21 +0700

    Create Workoutside.js

---
+++ src/pages/Workoutside/Workoutside.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Workoutside.scss'
+
+
+class Workoutside extends React.Component {
+
+  render() {
+    
+    return(
+    <div>
+    <h3 className="row">Workoutside Page</h3>
+    </div>
+    ) 
+  }
+}
+
+Workoutside.defaultProps = {}
+
+Workoutside.propTypes = {}
+
+export default Workoutside
