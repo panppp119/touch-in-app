d8028ad066a9f858edd5e0a539e69c60012baaa6
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-28 16:04:52 +0700

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import './Dashboard.scss'
-
-
-
-
 class Dashboard extends React.Component {
-
   render() {
-    
     return<h3 className="row">Dashboard Page</h3>
   }
 }
-
 Dashboard.defaultProps = {}
-
 Dashboard.propTypes = {}
-
 export default Dashboard
