6c92a1ba386c45a8a719bdd1c9e89369a9391496
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-28 16:04:50 +0700

    Update History.js

--- src/pages/Profile/History/History.js
+++ src/pages/Profile/History/History.js
@@ -1,37 +1,27 @@
 import React from 'react'
 import './History.scss'
 import { Link } from 'react-router-dom'
-
-
-
 class History extends React.Component {
-
   constructor() {
     super();
-
     var today = new Date(),
         date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-
     this.state = {
         date: date
     };
 }
-  render() {
-    
+  render() {  
     return(
     <div>
     <h3 className="row">Dashboard Page</h3>
       <div className="row">
       <h5>ประวัติการเช็คอิน</h5>
-      
         <div className="col-2">
          {this.state.date} 
-          </div>     
-          
+          </div>           
       </div>
       <img  className="search"  src={require('../../../img/search-logo.png')} />  
       <div className="border" />
-
       <Link to="/Workoutside">
       <div className="gradient">
             <img  className="image "  src={require('../../../img/workout.png')} />
@@ -41,9 +31,10 @@ class History extends React.Component {
             <div className="project"> เวลา 
             <p>สถานที่ </p>
             </div>
-          
+  
       </div>
       </Link>
+
       <Link to="/Workfromhome">
       <div className="gradient2">
             <img  className="image "  src={require('../../../img/workfromhome.png')} />
@@ -54,16 +45,12 @@ class History extends React.Component {
             <p>สถานที่ </p>
             </div>
       </div>
-      </Link>
- 
-          
+      </Link>  
+
     </div>
     ) 
   }
 }
-
 History.defaultProps = {}
-
 History.propTypes = {}
-
 export default History
