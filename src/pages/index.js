d97ab7294fb3bef9aedb30a2bb4cd05d38b1fca0
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-28 15:49:23 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
-
 import CoreLayout from 'components/layout/CoreLayout'
-
 import Dashboard from 'pages/Dashboard'
 import Login from 'pages/Login'
 import NoMatch from 'pages/NoMatch'
+import Workoutside from './Workoutside/Workoutside'
+import Workfromhome from './Workfromhome//Workfromhome'
+import History from './Profile/History/History'
 
 const routes = (
   <CoreLayout>
     <Switch>
+    <Route   path="/history" component={History} />
+      <Route   path="/Workoutside" component={Workoutside} />
+      <Route  path="/Workfromhome" component={Workfromhome} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/' component={Dashboard} />
       <Route component={NoMatch} />
