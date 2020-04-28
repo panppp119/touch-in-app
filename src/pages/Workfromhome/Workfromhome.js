d478963753534d9d1b913630b1fe181c984e5b30
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-28 14:50:18 +0700

    Create Workfromhome.js

---
+++ src/pages/Workfromhome/Workfromhome.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Workfromhome.scss'
+
+
+class Workfromhome extends React.Component {
+
+  render() {
+    
+    return(
+    <div>
+    <h3 className="row">Workfromhome Page</h3>
+    </div>
+    ) 
+  }
+}
+
+Workfromhome.defaultProps = {}
+
+Workfromhome.propTypes = {}
+
+export default Workfromhome
