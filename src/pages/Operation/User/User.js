4f9443ec622fa5285e9f65d28b96761c6717da62
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-29 18:29:07 +0700

    Update User.js

--- src/pages/Operation/User/User.js
+++ src/pages/Operation/User/User.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { BsPlusCircleFill } from "react-icons/bs";
 
 export class User extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='User-Import'>
+                <p>menubar</p>
+                <div className='text-title'>
+                    <h2>ผู้ใช้</h2>
+                    <div className='icon'>
+                        <BsPlusCircleFill />
+                    </div>
+                </div>
+                <div className='border' />
             </div>
         )
     }
