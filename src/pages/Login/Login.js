4b47c9012a3863fe1a07d8da95bcb4948a2d8fa7
Phitchaporn P <phitchaporn.p@touchtechnologies.co.th>
2020-04-29 08:44:09 +0700

    update styles

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,35 +1,37 @@
 import React from 'react'
-import Logo from 'components/Logo/Logo'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
 import firebase, { auth } from 'config/firebaseConfig'
-import Container from 'components/layout/Container'
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import touchLogo from 'images/touch_logo.png'
+
+import './Login.scss'
 
 class Login extends React.Component {
   state = { isSignedIn: false }
 
   uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      auth.GoogleAuthProvider.PROVIDER_ID
-    ],
+    signInFlow: 'popup',
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
     callbacks: {
-      signInSuccess: () => false
-    }
+      signInSuccess: () => false,
+    },
   }
 
   componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user => {
+    auth.onAuthStateChanged((user) => {
       this.setState({ isSignedIn: !!user })
-      console.log("user", user)
+      console.log('user', user)
     })
   }
 
   render() {
     return (
-      <Container>
-        <Logo />
-        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
-      </Container>
+      <div className='login-page'>
+        <div className='sign-in'>
+          <img src={touchLogo} alt='touch-logo' className='touch-logo' />
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={auth} />
+        </div>
+      </div>
     )
   }
 }
