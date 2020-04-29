c5654a2d97b66db05491b48feafb8a47b7146cbf
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-29 14:35:19 +0700

    Create importuser.js

---
+++ src/pages/Operation/ImportUser/importuser.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class ImportUser extends Component {
+    render() {
+        return (
+            <div className='import-user'>
+                <div className='text-title'>
+                    <h1>ผู้ใช้งาน</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default ImportUser
