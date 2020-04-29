b9e682060a4e3fa6d1e05d13eb2cca891bb97293
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-29 09:57:25 +0700

    Update Workfromhome.js

--- src/pages/Workfromhome/Workfromhome.js
+++ src/pages/Workfromhome/Workfromhome.js
@@ -3,13 +3,65 @@ import './Workfromhome.scss'
 
 
 class Workfromhome extends React.Component {
-
+  constructor() {
+    super();
+    var today = new Date(),
+        date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    this.state = {
+        date: date
+    };
+}
   render() {
     
     return(
-    <div>
-    <h3 className="row">Work From Home Page</h3>
-    </div>
+      <div>
+      <h3 className="row">Work From Home Page</h3>
+      <div className="row">
+        <h5>ประวัติการเช็คอิน</h5>
+          <div className="col-2">
+           {this.state.date} 
+            </div>           
+        </div>
+        <img  className="search"  src={require('../../img/search-logo.png')} />  
+        <div className="border" />
+  
+      
+        <div className="sumgroup2">
+        <div className="group">
+          <div className="group2"/>
+          <div className="font">
+          Work From Home
+          </div>
+        </div>
+        <div className="projectgroup">
+        <div className="projectgroup2"/>
+        <div className="fontproject">
+          โปรเจ็ค A
+        </div>
+        </div>
+        <div className="workoutdetail">
+     
+          สถานที่ 
+          <div className="linedetail"/>
+          ตำบล 
+          <div className="linedetail"/>
+          อำเภอ 
+          <div className="linedetail"/>
+          จังหวัด 
+          <div className="linedetail"/>
+          เวลา 
+          <div className="linedetail"/>
+          วันที่ 
+          <div className="linedetail"/>
+          พิกัด 
+          <div className="linedetail"/>
+          <div className="workcard"/>
+
+        </div>
+        </div>
+    
+  
+      </div>
     ) 
   }
 }
