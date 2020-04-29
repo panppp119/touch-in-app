801028698da57d0ae6b973ff664fad5aa60f5b1c
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-29 15:14:58 +0700

    Update importuser.js

--- src/pages/Operation/ImportUser/importuser.js
+++ src/pages/Operation/ImportUser/importuser.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Importuser extends Component {
+class Importuser extends React.Component {
     render() {
         return (
             <div>
