0b4d049bfc9078ccebaeb115952adb5eb3667e2f
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-29 15:27:57 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,11 @@ import CoreLayout from 'components/layout/CoreLayout'
 import Dashboard from 'pages/Dashboard'
 import Login from 'pages/Login'
 import NoMatch from 'pages/NoMatch'
-import Importuser from 'pages/Operation'
+
 
 const routes = (
   <CoreLayout>
     <Switch>
-      <Route exact path='/operation' component={Importuser} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/' component={Dashboard} />
       <Route component={NoMatch} />
