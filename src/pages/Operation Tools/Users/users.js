54e092c32c6ab5085cc663ebf5a916c214e65a26
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-04-30 16:19:12 +0700

    Update users.js

---
+++ src/pages/Operation Tools/Users/users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default Users
