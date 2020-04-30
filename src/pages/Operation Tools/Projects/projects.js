d2f4c9e701f3c02cfd64ef0136be8ce1179200ab
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-30 12:46:36 +0700

    Create projects.js

---
+++ src/pages/Operation Tools/Projects/projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './projects.scss'
+class projects extends React.Component {
+  render() {
+    return<h3 className="row">Import Project Page</h3>
+  }
+}
+projects.defaultProps = {}
+projects.propTypes = {}
+export default projects
