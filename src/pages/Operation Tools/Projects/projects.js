fc930bc7f82f35617b3bb2ba82085c267b1e20c5
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-04-30 13:04:16 +0700

    Update projects.js

--- src/pages/Operation Tools/Projects/projects.js
+++ src/pages/Operation Tools/Projects/projects.js
@@ -1,8 +1,54 @@
 import React from 'react'
 import './projects.scss'
+import { Link } from 'react-router-dom'
 class projects extends React.Component {
   render() {
-    return<h3 className="row">Import Project Page</h3>
+    return(
+        <div>
+        <div className="row">
+        <h5>Import Project Page</h5>
+          <div className="col-2">
+          
+            </div>           
+        </div>
+        <img  className="search"  src={require('../../../img/import.png')} />  
+        <div className="border" />
+  
+        <div className="gradient">
+              <img  className="image "  src={require('../../../img/project.png')} />
+              
+              <div className="work-project"> ไอดี 0001
+              <p>โปรเจค A</p>
+              </div>
+              <div className="project"> สถานะการทำงาน : กำลังทำงาน 
+              
+              </div>
+        </div>
+    
+  
+        <div className="gradient2">
+              <img  className="image "  src={require('../../../img/project.png')} />
+              <div className="work-project"> ไอดี 0002
+              <p>โปรเจค B</p>
+              </div>
+              <div className="project"> สถานะการทำงาน : กำลังทำงาน
+             
+              </div>
+        </div>
+
+        <div className="gradient2">
+              <img  className="image "  src={require('../../../img/project.png')} />
+              <div className="work-project"> ไอดี 0003
+              <p>โปรเจค C</p>
+              </div>
+              <div className="project"> สถานะการทำงาน : กำลังทำงาน
+             
+              </div>
+        </div>
+      
+  
+      </div>
+    )
   }
 }
 projects.defaultProps = {}
