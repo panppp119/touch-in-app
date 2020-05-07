66e4838b74099e2b9885bcd51a5025d60681761d
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-05-07 16:20:33 +0700

    Create checkIn.js

---
+++ src/pages/Check-In/checkIn.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './checkIn.scss'
+import { Link } from 'react-router-dom'
+//import moment from 'moment'
+class checkIn extends React.Component {
+ /* componentWillMount(){
+  
+    const timestamp = Date.now();
+    console.log(new Intl.DateTimeFormat(
+      'en-US', 
+      {year: 'numeric', 
+      month: '2-digit',
+      day: '2-digit', 
+      hour: '2-digit', 
+      minute: '2-digit',
+       second: '2-digit'}).format(timestamp));
+       //<h5>{moment(this.state.timestamp).format('DD/MM/YYYY, HH:mm')}</h5>
+  }*/
+  render() {  
+    return(
+    <div>
+      <div className="row">
+      <h5>Check-In Page</h5>
+        <div className="col-2">
+  
+          </div>           
+      </div>
+     
+      <div className="border" />
+      <br/>
+      <div className="workfromhome1">
+      <img  className="home"  src={require('../../img/home.png')} />
+      <div className="d1">
+        <h3>Work From Home</h3>
+      </div>
+      </div>
+      <div className="workfromhome2">
+      <img  className="workout"  src={require('../../img/workout.png')} />
+      <div className="d2">
+        <h3>Work Outside</h3>
+      </div>
+      </div>
+
+    
+
+    </div>
+    ) 
+  }
+}
+checkIn.defaultProps = {}
+checkIn.propTypes = {}
+export default checkIn
