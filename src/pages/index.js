aa5ab6d0ebc233e2b3e56a9724b0da6ad1c155a0
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-05-08 13:36:16 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,12 @@ import Login from 'pages/Login'
 import NoMatch from 'pages/NoMatch'
 import Profile from 'pages/Profile'
 
+
 const routes = (
   <CoreLayout>
     <Switch>
       <Route exact path='/login' component={Login} />
-
+     
       <Route exact path='/' component={Dashboard} />
       <Route path='/profile' component={Profile} />
 
