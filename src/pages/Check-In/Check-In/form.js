0b6fef9fffb326c04ff9fc886945e784a4a9020b
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-05-11 12:27:00 +0700

    Update form.js

--- src/pages/Check-In/Check-In/form.js
+++ src/pages/Check-In/Check-In/form.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
+import Container from 'components/layout/Container'
+import './form.scss'
+import { BsGeoAlt } from "react-icons/bs";
+import { BsDownload } from "react-icons/bs";
+import { BsCamera } from "react-icons/bs";
 
 
 export class Form extends Component {
     render() {
         return (
-            <div>
-                <h1>Test</h1>
-            </div>
+            <Container>
+                <div className='form-checkin'>
+                    <div className='text-title'>
+                        <h2>Work From Home</h2>
+                    </div>
+                    <div className='card-icon'>
+                    <div className='icon'>
+                            <BsDownload />
+                        </div>
+
+                        <div className='icon'>
+                            <BsCamera />
+                        </div>
+
+                        <div className='icon'>
+                            <BsGeoAlt />
+                        </div>
+                    </div>
+                       
+                    <div className='card-img' />
+                    <button type="button" className='btn btn-danger'>ลบ</button>
+                    <div className="card-detail">
+                        <p>เวลา </p>
+                        <div className="border-detail" />
+                        <p>สถานที่ </p>
+                    </div>
+                    <button type="button" className='btn btn-danger'>ยกเลิก</button>
+                    <button type="button" className='btn btn-success'>บันทึก</button>
+                    <button type="button" className='btn btn-primary'>รายละเอียด</button>
+                </div>
+            </Container>
         )
     }
 }
-
 
 export default Form
