f44380eaf799f9aca145543b4fa25bc15b1acc8f
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-05-11 11:47:13 +0700

    Create Workoutside.js

---
+++ src/pages/Workoutside/Workoutside.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Workoutside.scss'
+
+class Workoutside extends React.Component {
+  constructor() {
+    super()
+    var today = new Date(),
+      date =
+        today.getFullYear() +
+        '-' +
+        (today.getMonth() + 1) +
+        '-' +
+        today.getDate()
+    this.state = {
+      date: date,
+    }
+  }
+  render() {
+    return (
+      <div className='work-outside'>
+        <h3 className='row'>Workoutside Page</h3>
+        <div className='row'>
+          <h5>ประวัติการเช็คอิน</h5>
+          <div className='col-4'>{this.state.date}</div>
+        </div>
+
+        <div className='sumgroup'>
+          <div className='group'>
+            <div className='group2' />
+            <div className='font'>ปฏิบัติงานนอกสถานที่ จุดเริ่มต้น</div>
+          </div>
+          <div className='projectgroup'>
+            <div className='projectgroup2' />
+            <div className='fontproject'>โปรเจ็ค A</div>
+          </div>
+          <div className='workoutdetail'>
+            สถานที่
+            <div className='linedetail' />
+            ตำบล
+            <div className='linedetail' />
+            อำเภอ
+            <div className='linedetail' />
+            จังหวัด
+            <div className='linedetail' />
+            เวลา
+            <div className='linedetail' />
+            วันที่
+            <div className='linedetail' />
+            พิกัด
+            <div className='linedetail' />
+            <div className='card1' />
+            <div className='card2' />
+            <div className='card3' />
+          </div>
+
+          <div className='group'>
+            <div className='group2' />
+            <div className='font'>ปฏิบัติงานนอกสถานที่ จุดสิ้นสุด</div>
+          </div>
+          <div className='projectgroup'>
+            <div className='projectgroup2' />
+            <div className='fontproject'>โปรเจ็ค A</div>
+          </div>
+          <div className='workoutdetail'>
+            สถานที่
+            <div className='linedetail' />
+            ตำบล
+            <div className='linedetail' />
+            อำเภอ
+            <div className='linedetail' />
+            จังหวัด
+            <div className='linedetail' />
+            เวลา
+            <div className='linedetail' />
+            วันที่
+            <div className='linedetail' />
+            พิกัด
+            <div className='linedetail' />
+            <div className='card1' />
+            <div className='card2' />
+            <div className='card3' />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Workoutside.defaultProps = {}
+
+Workoutside.propTypes = {}
+
+export default Workoutside
