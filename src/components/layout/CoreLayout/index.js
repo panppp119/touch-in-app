8e17cbff2f8607401c7e425aa8b5a9e67aa9c764
Frong1593 <47934291+Frong1593@users.noreply.github.com>
2020-05-11 11:47:50 +0700

    Update index.js

--- src/components/layout/CoreLayout/index.js
+++ src/components/layout/CoreLayout/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 
 import Menu from 'components/nav/Menu/Menu'
 import Container from 'components/layout/Container'
@@ -21,10 +22,16 @@ class CoreLayout extends React.Component {
     return (
       <div id='corelayout'>
         <nav className='layout-header'>
-          <Menu visibility={showMenu} setVisibility={this.setShowMenu} />
+          <Menu
+            visibility={showMenu}
+            setVisibility={this.setShowMenu}
+            {...this.props}
+          />
         </nav>
 
-        <Container showMenu={showMenu}>{this.props.children}</Container>
+        <Container showMenu={showMenu} {...this.props}>
+          {this.props.children}
+        </Container>
 
         <footer className='layout-footer'></footer>
       </div>
@@ -38,4 +45,4 @@ CoreLayout.propTypes = {
   children: PropTypes.node,
 }
 
-export default CoreLayout
+export default withRouter(CoreLayout)
