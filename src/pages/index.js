3057b679c61039f53598f1ae6fb59cf5bfe69b69
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-05-13 00:04:03 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,16 @@ import Workfromhome from './Workfromhome//Workfromhome'
 import Profile from 'pages/Profile'
 import CheckIn from 'pages/CheckIn'
 import Report from 'pages/Report'
+import ReportWFH from './Report/Report-WFH/ReportWFH'
+import ReportWFO from './Report/Report-WFO/ReportWFO'
 
 const routes = (
   <CoreLayout>
     <Switch>
       <Route path='/Workoutside' component={Workoutside} />
       <Route path='/Workfromhome' component={Workfromhome} />
+      <Route path='/Report/Report-WFH' component={ReportWFH} />
+      <Route path='/Report/Report-WFO' component={ReportWFO} />
       <Route exact path='/login' component={Login} />
 
       <Route exact path='/' component={Dashboard} />
