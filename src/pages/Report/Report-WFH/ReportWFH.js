f659e226a498a93fce9a76946b7dd72e5a00ff54
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-05-13 00:04:09 +0700

    Create ReportWFH.js

---
+++ src/pages/Report/Report-WFH/ReportWFH.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { BsFillHouseFill } from "react-icons/bs";
+import './ReportWFH.scss'
+
+
+export class ReportWFH extends Component {
+
+    render() {
+        return (
+            <div className='report-wfh'>
+                <div className='text-title'>
+                    <h1>ประวัติการเช็คอิน</h1>
+                </div>
+                <div className='border' />
+                <div className='title-bar'>
+                    <h3>
+                        <div className='title-icon'>
+                            <BsFillHouseFill />
+                        </div>
+                        Work From Home
+                    </h3>
+                </div>
+                <div className='card-detail'>
+                    <p>ลำดับที่ :</p>
+                    <div className='border-detail' />
+                    <p>รหัสพนักงาน :</p>
+                    <div className='border-detail' />
+                    <p>ชื่อ - นามสกุล :</p>
+                    <div className='border-detail' />
+                    <p>แผนก :</p>
+                    <div className='border-detail' />
+                    <p>ประวัติการเช็คอิน :</p>
+                    <div className='border-detail' />
+                    <p>โปรเจค :</p>
+                    <div className='border-detail' />
+                    <p>ตำบล :</p>
+                    <div className='border-detail' />
+                    <p>อำเภอ :</p>
+                    <div className='border-detail' />
+                    <p>จังหวัด :</p>
+                    <div className='border-detail' />
+                    <p>วันที่ :</p>
+                    <div className='border-detail' />
+                    <p>เวลา :</p>
+                    <div className='border-detail' />
+                    <p>ตำแหน่ง :</p>
+                    <div className='border-detail' />
+                    <p>รูปภาพ :
+                        <div className='workcard' />
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReportWFH.defaultProps = {}
+
+ReportWFH.propTypes = {}
+
+export default ReportWFH
