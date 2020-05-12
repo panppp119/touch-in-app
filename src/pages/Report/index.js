fe3bbcc041d3391eaef520a6408fc694fa8f5ea3
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-05-12 13:50:39 +0700

    Update index.js

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,18 +1,3 @@
-import React from 'react'
-import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import Report from './Report'
 
-import CheckInList from './CheckInList'
-// import CheckInView from './CheckInView'
-
-const Profile = () => {
-  const { path } = useRouteMatch()
-
-  return (
-    <Switch>
-      <Route exact path={`${path}`} component={CheckInList} />
-      {/* <Route exact path={`${path}/:id`} component={CheckInView} /> */}
-    </Switch>
-  )
-}
-
-export default Profile
+export default Report
