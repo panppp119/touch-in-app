205f10051b983e476de5a91472b6acfb26248fdb
Jai.Pornpawit <62242117+jai-pornpawit@users.noreply.github.com>
2020-05-13 17:22:58 +0700

    Update index.js

--- src/components/filter/ReportFilter/index.js
+++ src/components/filter/ReportFilter/index.js
@@ -43,6 +43,86 @@ const Filter = styles.div`
         margin-right: 20px;
       }
     }
+    .btn {
+      text-align: center;
+      vertical-align: middle;
+      cursor: pointer;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      background-color: transparent;
+      border: 1px solid transparent;
+      padding: 0.375rem 0.75rem;
+      font-size: 1rem;
+      line-height: 1.5;
+      border-radius: 0.25rem;        
+  }
+    .btn:focus, .btn.focus {
+      outline: 0;
+      box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    }
+  
+    .btn-danger {
+      margin-top: 10px;
+      color: #fff;
+      background-color: #dc3545;
+      border-color: #dc3545;
+    }
+    
+    .btn-danger:hover {
+      color: #fff;
+      background-color: #c82333;
+      border-color: #bd2130;
+    }
+    
+    .btn-danger:focus, .btn-danger.focus {
+      color: #fff;
+      background-color: #c82333;
+      border-color: #bd2130;
+      box-shadow: 0 0 0 0.2rem rgba(225, 83, 97, 0.5);
+    }
+
+    .btn-light {
+      margin-top: 10px;
+      color: #fff;
+      background-color: #343a40;
+      border-color: #343a40;
+    }
+    
+    .btn-light:hover {
+      color: #fff;
+      background-color: #23272b;
+      border-color: #1d2124;
+    }
+    
+    .btn-light:focus, .btn-primary.focus {
+      color: #fff;
+      background-color: #23272b;
+      border-color: #1d2124;
+      box-shadow: 0 0 0 0.2rem rgba(82, 88, 93, 0.5);
+    }
+
+    .btn-success {
+      margin-top: 10px;
+      color: #fff;
+      background-color: #28a745;
+      border-color: #28a745;
+    }
+    
+    .btn-success:hover {
+      color: #fff;
+      background-color: #218838;
+      border-color: #1e7e34;
+    }
+    
+    .btn-success:focus, .btn-success.focus {
+      color: #fff;
+      background-color: #218838;
+      border-color: #1e7e34;
+      box-shadow: 0 0 0 0.2rem rgba(72, 180, 97, 0.5);
+    }
+
   }
 `
 
@@ -87,8 +167,9 @@ const ReportFilter = () => {
       </div>
 
       <div className='actions'>
-        <button>Clear</button>
-        <button>Search</button>
+        <button type="button" className='btn btn-light'>พิมพ์รายงาน</button>
+        <button type="button" className='btn btn-success'>ค้นหา</button>
+        <button type="button" className='btn btn-danger'>ยกเลิก</button>
       </div>
     </Filter>
   )
